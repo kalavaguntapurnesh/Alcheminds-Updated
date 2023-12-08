25532b393796f323da7215fad447fa3d26c379ee
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2023-12-08 12:34:48 +0530

    Seventh Update

---
+++ src/app/components/One.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { MdOutlineCastForEducation } from "react-icons/md";
+import { SiHomeassistantcommunitystore } from "react-icons/si";
+import { GrTechnology } from "react-icons/gr";
+import { GrAd } from "react-icons/gr";
+
+
+const One = () => {
+  return (
+    <div className="bg-gray-50">
+      <div className="relative pt-8 pb-8">
+        <div className="w-full space-y-10 lg:space-y-20">
+          <div className="relative grid items-center w-full grid-cols-1 gap-16 px-4 mx-auto lg:gap-16 lg:grid-cols-3 max-w-7xl ">
+            <div className=" flex flex-row space-x-4 justify-center items-center w-full">
+                <div>
+                    <MdOutlineCastForEducation className="w-[40px] h-[40px] text-companyColor" />
+                </div>
+                <div className="text-lg leading-7 text-gray-500">
+                    Improvised Study Platform
+                </div>
+            </div>
+            <div className=" flex flex-row space-x-4 justify-center items-center w-full">
+                <div>
+                    <GrTechnology className="w-[40px] h-[40px] text-companyColor" />
+                </div>
+                <div className="text-lg leading-7 text-gray-500">
+                    Patient Health Management 
+                </div>
+            </div>
+            <div className=" flex flex-row space-x-4 justify-center items-center w-full">
+                <div>
+                    <GrAd className="w-[40px] h-[40px] text-companyColor" />
+                </div>
+                <div className="text-lg leading-7 text-gray-500">
+                    Tracking Billable Hours
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default One;
