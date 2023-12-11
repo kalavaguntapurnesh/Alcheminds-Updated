a24a6bc9ec129f8b3a5919399153c9e978ec62c8
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2023-12-11 10:31:41 +0530

    Twelve

--- src/app/about/First.jsx
+++ src/app/about/First.jsx
@@ -9,10 +9,10 @@ const First = () => {
           <div className="w-full px-4 mx-auto max-w-7xl">
             <div className="justify-center w-full text-center pb-14">
               <div className="w-full max-w-5xl mx-auto space-y-4">
-                <h3 className="text-4xl font-semibold tracking-tight dark:text-white">
+                <h3 className="text-4xl font-semibold tracking-tight dark:text-black">
                   About <span className="text-companyColor">Us</span>
                 </h3>
-                <div className="prose-lg text-gray-600 dark:text-white">
+                <div className="prose-lg text-gray-600 dark:text-gray-800">
                   <p>
                     Alcheminds Solutions is global software, back office
                     services and S/W products/applications Company incorporated
